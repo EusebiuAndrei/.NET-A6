fc4741a53d1e2fa34e57881c5a03102bdf4bde8d
Ana-MariaZanceanu <ana.zanceanu@yahoo.com>
2020-12-02 19:37:24 +0200

    bbc crawler sport and health

---
+++ Crawler/CrawlerAPI/Controllers/NewsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CrawlerAPI.CrawlingFunctionsBBC;
+using CrawlerAPI.NewsModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrawlerAPI.Controllers
+{
+    [Route("api/news")]
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+        [HttpGet("bbc/sport")]
+        public async Task<ActionResult<List<News>>> GetSportNewsFromBBC()
+        {
+            SportCrawler sportCrawler = new SportCrawler();
+            return await sportCrawler.StartCrawlerAsync();
+        }
+
+        [HttpGet("bbc/health")]
+        public async Task<ActionResult<List<News>>> GetHealthNewsFromBBC()
+        {
+            HealthCrawler healthCrawler = new HealthCrawler();
+            return await healthCrawler.StartCrawlerAsync();
+        }
+    }
+}
---
+++ Crawler/CrawlerAPI/NewsModel/News.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CrawlerAPI.NewsModel
+{
+    public class News
+    {
+        public string Title { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+        public string SourceLink { get; set; }
+    }
+}
