69b251a5d8d1f4a30e2633c6b9cf7746d6345025
Gitea <gitea@fake.local>
2020-12-03 20:01:21 +0200

    Add swagger and appsettings for each environment

--- backend/NewsAPI/API/Startup.cs
+++ backend/NewsAPI/API/Startup.cs
@@ -1,17 +1,13 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -33,6 +29,34 @@ namespace API
             services.AddScoped<INewsTopicRepository, NewsTopicRepository>();
             services.AddScoped<ITopicRepository, TopicRepository>();
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.CustomOperationIds(e => $"{e.ActionDescriptor.RouteValues["controller"]}_{e.ActionDescriptor.RouteValues["action"]}_{e.HttpMethod}");
+                c.SchemaFilter<CamelCasingPropertiesFilter>();
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "News API V1", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please insert JWT with Bearer into field",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                   {
+                     new OpenApiSecurityScheme
+                     {
+                       Reference = new OpenApiReference
+                       {
+                         Type = ReferenceType.SecurityScheme,
+                         Id = "Bearer"
+                       }
+                      },
+                      new string[] { }
+                    }
+                  });
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +65,12 @@ namespace API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "News API V1");
+                    c.RoutePrefix = "swagger";
+                });
             }
 
             app.UseHttpsRedirection();
@@ -53,6 +83,15 @@ namespace API
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private class CamelCasingPropertiesFilter : ISchemaFilter
+        {
+            public void Apply(OpenApiSchema model, SchemaFilterContext context)
+            {
+                model.Properties =
+                    model.Properties.ToDictionary(d => d.Key.Substring(0, 1).ToLower() + d.Key.Substring(1), d => d.Value);
+            }
         }
     }
 }
