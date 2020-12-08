bb0fb59eb56e3e641a342a92fc4006acda7ad73c
Stan Laura-Ioana <laurastan1999@gmail.com>
2020-12-08 14:40:19 +0200

    CBSNews crawler

--- Crawler/CrawlerAPI/CrawlingFunctionsCBSNews/CrawlerCBS.cs
+++ Crawler/CrawlerAPI/CrawlingFunctionsCBSNews/CrawlerCBS.cs
@@ -1,5 +1,5 @@
-﻿using CrawlerAPI.NewsModel;
-using HtmlAgilityPack; 
+using CrawlerAPI.NewsModel;
+using HtmlAgilityPack;  
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace CrawlerAPI.CrawlingFunctionsBBC
 {
-    public static class CrawlerBBC
+    public static class CrawlerCBS
     {
         public static async Task<List<News>> StartCrawlerAsync(string url, string[] newsDivsClasses, string subject)
         {
@@ -23,28 +23,14 @@ namespace CrawlerAPI.CrawlingFunctionsBBC
             var divs = htmlDocument.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("class", "").Equals(newsDivsClasses[0])).ToList();
             for(int i = 1; i < newsDivsClasses.Length; i++)
             {
-                divs.AddRange(htmlDocument.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("class", "").Equals(newsDivsClasses[i])).ToList());
+                divs.AddRange(htmlDocument.DocumentNode.Descendants("article").Where(node => node.GetAttributeValue("class", "").Equals(newsDivsClasses[i])).ToList());
             }
             foreach (var div in divs)
             {
-                string title = HtmlEntity.DeEntitize(div.Descendants("h3").FirstOrDefault().InnerText);
+                string title = HtmlEntity.DeEntitize(div.Descendants("h4").FirstOrDefault().InnerText);
                 var descendantA = div.Descendants("a").FirstOrDefault();
                 var sourceLink = descendantA.ChildAttributes("href").FirstOrDefault().Value;
-                if (!sourceLink.StartsWith("https://www.bbc.co.uk"))
-                {
-                    sourceLink = "https://www.bbc.co.uk" + sourceLink;
-                }
-                try
-                {
-                    if(descendantA.Descendants("span").FirstOrDefault().ChildAttributes("class").FirstOrDefault().Value == "gs-o-bullet gs-c-live-pulse gs-c-live-pulse--sport gs-u-mr")
-                    {
-                        continue;
-                    }
-                }
-                catch(Exception e)
-                {
-                    //Console.WriteLine(e);
-                }
+
                 var newsHtml = await httpClient.GetStringAsync(sourceLink);
                 var newsHtmlDocument = new HtmlDocument();
                 newsHtmlDocument.LoadHtml(newsHtml);
@@ -52,26 +38,19 @@ namespace CrawlerAPI.CrawlingFunctionsBBC
                 var date = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss");
                 try
                 {
-                    article = newsHtmlDocument.DocumentNode.Descendants("article").Where(node => !node.GetAttributeValue("class", "").Contains("sp-c-fixture")).FirstOrDefault();
+                    article = newsHtmlDocument.DocumentNode.Descendants("article").Where(node => node.GetAttributeValue("class", "").Equals("content content-article")).FirstOrDefault();
                     date = article.Descendants("time").FirstOrDefault().ChildAttributes("datetime").FirstOrDefault().Value;
                 }
-                catch (Exception e)
+                catch(Exception e)
                 {
-                    //Console.WriteLine(e);
+                    Console.WriteLine(e);
                     continue;
                 }
+                var sectionContent = article.Descendants("section").Where(node => node.GetAttributeValue("class", "").Equals("content__body")).FirstOrDefault();
                 StringBuilder concatenateParagraphs = new StringBuilder();
-                var textElements = new string[]{"h1", "h2", "h3", "h4", "h5", "h6", "p", "li"};
-                foreach (var item in article.DescendantsAndSelf())
+                var textElements = new string[] { "h1", "h2", "h3", "h4", "h5", "h6", "p", "li" };
+                foreach (var item in sectionContent.DescendantsAndSelf())
                 {
-                    if (item.Name == "header" || item.ParentNode.Name == "header")
-                    {
-                        continue;
-                    }
-                    if(item.Id.Contains("comments"))
-                    {
-                        break;
-                    }
                     if (textElements.Contains(item.Name))
                     {
                         if (item.InnerText.Trim() != "")
@@ -85,7 +64,7 @@ namespace CrawlerAPI.CrawlingFunctionsBBC
                     Title = title,
                     Subject = subject,
                     Content = concatenateParagraphs.ToString(),
-                    Date = DateTime.ParseExact(date.Substring(0, 19), "yyyy-MM-ddTHH:mm:ss", CultureInfo.InvariantCulture),
+                    Date = Convert.ToDateTime(date),
                     SourceLink = sourceLink,
                 };
                 newsList.Add(news);
