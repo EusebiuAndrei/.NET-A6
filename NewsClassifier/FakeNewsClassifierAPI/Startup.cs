61786427e02d67d400fe15de8ed233d23b288c4a
Ana-MariaZanceanu <ana.zanceanu@yahoo.com>
2020-12-10 18:50:30 +0200

    add swagger to fake news api

--- NewsClassifier/FakeNewsClassifierAPI/Startup.cs
+++ NewsClassifier/FakeNewsClassifierAPI/Startup.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
 
 namespace FakeNewsClassifierAPI
 {
@@ -19,6 +22,33 @@ namespace FakeNewsClassifierAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.CustomOperationIds(e => $"{e.ActionDescriptor.RouteValues["controller"]}_{e.ActionDescriptor.RouteValues["action"]}_{e.HttpMethod}");
+                c.SchemaFilter<CamelCasingPropertiesFilter>();
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ML News API V1", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please insert JWT with Bearer into field",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                   {
+                     new OpenApiSecurityScheme
+                     {
+                       Reference = new OpenApiReference
+                       {
+                         Type = ReferenceType.SecurityScheme,
+                         Id = "Bearer"
+                       }
+                      },
+                      new string[] { }
+                    }
+                  });
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,6 +57,12 @@ namespace FakeNewsClassifierAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ML News API V1");
+                    c.RoutePrefix = "swagger";
+                });
             }
 
             app.UseRouting();
@@ -37,6 +73,15 @@ namespace FakeNewsClassifierAPI
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private class CamelCasingPropertiesFilter : ISchemaFilter
+        {
+            public void Apply(OpenApiSchema model, SchemaFilterContext context)
+            {
+                model.Properties =
+                    model.Properties.ToDictionary(d => d.Key.Substring(0, 1).ToLower() + d.Key.Substring(1), d => d.Value);
+            }
         }
     }
 }
